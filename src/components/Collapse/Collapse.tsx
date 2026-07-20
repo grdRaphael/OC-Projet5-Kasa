84ed2f82bf64b4c1bc41cbbf447bac3ad9d7d926
grdRaphael <drebin235@gmail.com>
2026-07-20 17:21:23 +0400

    Make Collapse keyboard accessible with button and aria-expanded

--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -9,16 +9,22 @@ type CollapseProps = {
     children: React.ReactNode;
 }
 
-const Collapse = ({ title, children,  }: CollapseProps) => {
+const Collapse = ({ title, children }: CollapseProps) => {
     const [isOpen, setIsOpen]= useState(false)
 
     return (
         <div className='collapse_wrapper'>
-            <div className="collapse"
-                onClick={() => setIsOpen(!isOpen)}>
-                <h3 className='collapse_title'>{title}</h3>
-                <img src={isOpen ? vectorUp : vectorDown } alt="icon" />
-            </div>
+            <h3 className='collapse_heading'>
+                <button
+                    type="button"
+                    className="collapse"
+                    onClick={() => setIsOpen(!isOpen)}
+                    aria-expanded={isOpen}
+                >
+                    <span className='collapse_title'>{title}</span>
+                    <img src={isOpen ? vectorUp : vectorDown } alt="" />
+                </button>
+            </h3>
             {isOpen && <CollapsePanel>{children}</CollapsePanel>}
         </div>
 
